4498fb7f934b55c4cc13afb68616d5ec8fe3a7f1
Shabbir khan <33508921+shabbirkhan0015@users.noreply.github.com>
2025-05-25 06:08:50 +0530

    harkirtCohort 0-1

---
+++ class1/main.js
@@ -0,0 +1,69 @@
+// function printname(firstname, lastname) {
+//   console.log(firstname + " " + lastname);
+// }
+// printname("shabbir", "khan");
+
+// function namebasedongender(name, gender)
+// {
+//     if(gender == "male")
+//     {
+//         console.log("He is "+name);
+
+//     }
+//     else{
+//         console.log("She is "+name)
+//     }
+// }
+// namebasedongender("Shabbir", "male");
+// namebasedongender("Kriti Sanon", "female");
+
+// for (var i = 0; i <= 1000; i++) {
+//   console.log(i);
+// }
+// const numbers1 = [1,3,5,67,8,85,44,7,30, 54, 21, 42];
+
+// for(var i =0; i<numbers.length;i++)
+// {
+//     if(numbers[i] %2 == 0)
+//     {
+//         console.log(numbers[i])
+//     }
+// }
+
+// const numbers2 = [1, 3, 5, 67, 8, 85, 44, 7, 30, 54, 21, 42];
+// let max = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//   if (numbers[i]> max) {
+//     max=numbers[i];
+
+//   }
+// }
+// console.log(max)
+
+// const numbers = [1, 3, 5, 67, 8, 85, 44, 7, 21, 42];
+
+// for (var i = numbers.length - 1; i >= 0; i--) {
+//   console.log(numbers[i]);
+// }
+
+// const users = [
+//   {
+//     firstName: "Shabbir",
+//     Gender: "Male",
+//   },
+//   {
+//     firstName: "Kriti Sanon",
+//     Gender: "Female",
+//   },
+// ];
+
+// for (let i = 0; i < users.length; i++) {
+//   if (users[i]["Gender"] == "Male") {
+//     console.log(users[i]["firstName"]);
+//   }
+// }
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5, 6));
